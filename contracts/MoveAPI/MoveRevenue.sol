191b80f3d3b13a1e1e37ac3f9d6d42cf2365e9eb
SuJustin <su.justin.developer@gmail.com>
2021-03-31 22:05:08 +0800

    v2.1 +contract(0xbCB90Fa44AD0Ba8871a72aE07cbe9Fd2b387007a) |(transferChainlinkAPIKey)

--- contracts/MoveAPI/MoveRevenue.sol
+++ contracts/MoveAPI/MoveRevenue.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.5/ChainlinkClient.sol";
 import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.5/vendor/Ownable.sol";
 
+// v2.1
 contract MoveRevenue is ChainlinkClient, Ownable {
     uint256 private constant ORACLE_PAYMENT = 1 * LINK;
     string private apikey;
@@ -11,15 +12,15 @@ contract MoveRevenue is ChainlinkClient, Ownable {
         bytes32 indexed requestId,
         uint256 indexed movieRevenue
     );
-    event RequestEthereumPriceFulfilled(
-        bytes32 indexed requestId,
-        uint256 indexed price
+    event APIKeyTransferred(
+        string indexed previousAPIKey,
+        string indexed apiKey
     );
 
     constructor(
         string memory _apikey,
-        address _orcale,
-        address _chainLinkToken
+        address _chainLinkToken,
+        address _orcale
     ) public Ownable() {
         apikey = _apikey;
         setChainlinkToken(_chainLinkToken);
@@ -51,59 +52,6 @@ contract MoveRevenue is ChainlinkClient, Ownable {
         currentMoveRevenue = _movieRevenue;
     }
 
-    // Chainlink Example
-    uint256 public currentPrice;
-
-    // Creates a Chainlink request with the uint256 multiplier job and returns the requestId
-    function requestEthereumPrice(string memory _jobId, string memory _currency)
-        public
-        returns (bytes32 requestId)
-    {
-        // newRequest takes a JobID, a callback address, and callback function as input
-        Chainlink.Request memory req =
-            buildChainlinkRequest(
-                stringToBytes32(_jobId),
-                address(this),
-                this.fulfillEthereumPrice.selector
-            );
-        // Adds a URL with the key "get" to the request parameters
-        req.add(
-            "get",
-            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR,JPY"
-        );
-        // Uses input param (dot-delimited string) as the "path" in the request parameters
-        req.add("path", _currency);
-        // Adds an integer with the key "times" to the request parameters
-        req.addInt("times", 100);
-        // Sends the request with 1 LINK to the oracle contract
-        requestId = sendChainlinkRequest(req, 1 * LINK);
-    }
-
-    // fulfillEthereumPrice receives a uint256 data type
-    function fulfillEthereumPrice(bytes32 _requestId, uint256 _price)
-        public
-        // Use recordChainlinkFulfillment to ensure only the requesting oracle can fulfill
-        recordChainlinkFulfillment(_requestId)
-    {
-        currentPrice = _price;
-    }
-
-    function getChainlinkToken() public view returns (address) {
-        return chainlinkTokenAddress();
-    }
-
-    function getChainlinkOracle() public view returns (address) {
-        return chainlinkOracleAddress();
-    }
-
-    function withdrawLink() public onlyOwner {
-        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-        require(
-            link.transfer(msg.sender, link.balanceOf(address(this))),
-            "Unable to transfer"
-        );
-    }
-
     function cancelRequest(
         bytes32 _requestId,
         uint256 _payment,
@@ -118,6 +66,35 @@ contract MoveRevenue is ChainlinkClient, Ownable {
         );
     }
 
+    // withdraw
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+
+    // get/set
+    function getChainlinkToken() public view returns (address) {
+        return chainlinkTokenAddress();
+    }
+
+    function getChainlinkOracle() public view returns (address) {
+        return chainlinkOracleAddress();
+    }
+
+    function transferChainlinkAPIKey(string memory newAPIKey) public onlyOwner {
+        _transferChainlinkAPIKey(newAPIKey);
+    }
+
+    function _transferChainlinkAPIKey(string memory newAPIKey) internal {
+        require(bytes(newAPIKey).length != 0, "APIKey: api key is empty.");
+        emit APIKeyTransferred(apikey, newAPIKey);
+        apikey = newAPIKey;
+    }
+
+    // Tool
     function stringToBytes32(string memory source)
         private
         pure
@@ -139,15 +116,6 @@ contract MoveRevenue is ChainlinkClient, Ownable {
         returns (string memory)
     {
         string memory url = "https://api.themoviedb.org/3/movie/";
-        return
-            string(
-                abi.encodePacked(
-                    url,
-                    _moveId,
-                    "?api_key=",
-                    apikey,
-                    "&language=zh-TW"
-                )
-            );
+        return string(abi.encodePacked(url, _moveId, "?api_key=", apikey));
     }
 }
